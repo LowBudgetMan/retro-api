91eabfd4b6ca4d3f368605517b37822b92d18837
Nick Reuter <thenewimagineer@gmail.com>
2024-04-23 00:08:37 -0400

    Initial commit

---
+++ src/main/java/io/nickreuter/retroapi/RetroApiApplication.java
@@ -0,0 +1,13 @@
+package io.nickreuter.retroapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetroApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetroApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/nickreuter/retroapi/RetroApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.nickreuter.retroapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetroApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
