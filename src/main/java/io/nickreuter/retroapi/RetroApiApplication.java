2bdc35816080fb48e001eef14832411e0e94f591
Nick Reuter <thenewimagineer@gmail.com>
2025-11-11 16:07:29 -0500

    Add autoconfiguration to root application

--- src/main/java/io/nickreuter/retroapi/RetroApiApplication.java
+++ src/main/java/io/nickreuter/retroapi/RetroApiApplication.java
@@ -1,11 +1,13 @@
 package io.nickreuter.retroapi;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
+@AutoConfiguration
 public class RetroApiApplication {
 
 	public static void main(String[] args) {
