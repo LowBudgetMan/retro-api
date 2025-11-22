b3e9ddf43766a2182f9dad433e6b0accdfcb936c
Nick Reuter <thenewimagineer@gmail.com>
2025-11-22 10:25:20 -0500

    Add Custom implementation for JWT decoder based on typ property

--- src/main/java/io/nickreuter/retroapi/configuration/SecurityConfig.java
+++ src/main/java/io/nickreuter/retroapi/configuration/SecurityConfig.java
@@ -1,12 +1,14 @@
 package io.nickreuter.retroapi.configuration;
 
 import io.nickreuter.retroapi.configuration.environment.CorsConfig;
+import io.nickreuter.retroapi.configuration.jwt.AllTypeJwtDecoderFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -17,10 +19,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class SecurityConfig implements WebMvcConfigurer {
 
     private final CorsConfig corsConfig;
+    private final AllTypeJwtDecoderFactory decoderFactory;
 
-    public SecurityConfig(CorsConfig corsConfig) {
+    public SecurityConfig(CorsConfig corsConfig, AllTypeJwtDecoderFactory decoderFactory) {
         this.corsConfig = corsConfig;
+        this.decoderFactory = decoderFactory;
     }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
@@ -32,7 +37,9 @@ public class SecurityConfig implements WebMvcConfigurer {
                     authorize.requestMatchers("/api/websocket").permitAll();
                     authorize.anyRequest().authenticated();
                 })
-                .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()));
+                .oauth2ResourceServer((oauth2) -> oauth2.jwt(jwt -> {
+                    jwt.jwtAuthenticationConverter(jwtAuthenticationConverter());
+                }));
         return http.build();
     }
 
@@ -44,5 +51,10 @@ public class SecurityConfig implements WebMvcConfigurer {
                 .allowedHeaders("*")
                 .exposedHeaders("*")
                 .allowCredentials(true).maxAge(3600);
+    }
+
+    @Bean
+    public JwtAuthenticationConverter jwtAuthenticationConverter() {
+        return new JwtAuthenticationConverter();
     }
 }
