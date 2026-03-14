374df47e5eaee7ae7a81f7bebea395f5cc15686b
Nick Reuter <thenewimagineer@gmail.com>
2026-03-14 11:58:55 -0400

    feat: add ephemeral event types to EventType enum

--- src/main/java/io/nickreuter/retroapi/notification/EventType.java
+++ src/main/java/io/nickreuter/retroapi/notification/EventType.java
@@ -3,5 +3,12 @@ package io.nickreuter.retroapi.notification;
 public enum EventType {
     CREATE,
     UPDATE,
-    DELETE
+    DELETE,
+    TIMER_START,
+    TIMER_STOP,
+    FOCUS,
+    FOCUS_CLEAR,
+    SORT,
+    PHASE,
+    RETRO_FINISHED
 }
